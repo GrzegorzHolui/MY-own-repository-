f39bea8b6af9efa13b2209438ad102022640b02b
Grzegorz <grzesiu.holui@gmail.com>
2020-04-04 14:48:56 +0200

    Add with test

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 
 int main()
 {
-  List<std::string> list;
-  list.push_back("XD");
-  list.push_back("69");
-  list.push_back("420");
-  list.print();
+  // List<std::string> list;
+  // list.push_back("XD");
+  // list.push_back("69");
+  // list.push_back("420");
+  // list.print();
   //std::cout<<std::endl <<"Size: " <<list.size()<<std::endl;
   return 0;
 }
