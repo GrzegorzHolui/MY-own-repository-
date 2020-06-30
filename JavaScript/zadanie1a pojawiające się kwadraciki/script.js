58f730ff4905797f2f225b94a7c42aabf0d8aa55
Grzegorz <grzesiu.holui@gmail.com>
2020-06-30 11:11:50 +0200

    add zadanie 1 pojawiające się kwadraty

---
+++ JavaScript/zadanie1a pojawiające się kwadraciki/script.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector("button");
+const head = document.querySelector('header');
+// function addDiv () {
+//     const div = document.createElement('div');
+//     if (interior % 5 == 0) {
+//         div.style.borderRadius = '50%';
+//     }
+//     div.textContent = interior;
+//     ++interior; 
+//     head.appendChild(div);
+
+// }
+// let interior = 1;
+//btn.addEventListener("click", addDiv);
+// let addDiv = function () {
+//     const div = document.createElement('div');
+//     if (interior % 5 == 0) {
+//         div.style.borderRadius = '50%';
+//     }
+//     div.textContent = interior;
+//     ++interior;
+//     head.appendChild(div);
+
+// }
+//btn.addEventListener("click", addDiv);
+// let interior = 1;
+
+// btn.addEventListener("click", (()=>{
+//     const div = document.createElement('div');
+//     if (interior % 5 == 0) {
+//         div.style.borderRadius = '50%';
+//     }
+//     div.textContent = interior;
+//     ++interior;
+//     head.appendChild(div);
+// }))
